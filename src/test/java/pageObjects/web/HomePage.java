e5bc57548c917fea1a16de6c2891e93530198d30
unknown <realtimeonline>
2025-07-27 09:47:59 +0530

    Updated Jenkins file

--- src/test/java/pageObjects/web/HomePage.java
+++ src/test/java/pageObjects/web/HomePage.java
@@ -6,6 +6,6 @@ public class HomePage {
 
 	public By buttonconfirm= By.xpath("//div[text()='Confirm']");
 	public By linkElectricBikes = By.linkText("Electric Bikes");
-	public By linkAdventureNeo = By.xpath("//a[text()='Adventure Neo']]");
+	public By linkAdventureNeo = By.xpath("//a[text()='Adventure Neo']");
 
 }
